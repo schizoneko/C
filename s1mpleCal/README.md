53a29cded0a89db215e60d51aef9365d6b264429
schizoneko <viettung11122004@gmail.com>
2024-05-22 19:30:58 +0700

    [ADD] add initial UI for user

---
+++ s1mpleCal/README.md
@@ -0,0 +1,9 @@
+# Simple Calculator
+## Basic function
+### 1. Basic Calculation 
+- Input: 2 integers, 1 symbol
+- Output: Value
+### 2. Find the GCD
+- Input: 2 integers
+- Output: GCD
+### 3. Exit
---
+++ s1mpleCal/main.c
@@ -0,0 +1,12 @@
+#include "stdio.h"
+
+int main(){
+    printf("\nSimple Calculator");
+
+    printf("\n1. Basic Calculator");
+    printf("\n2. GCD");
+    printf("\n3. Exit");
+
+    printf("\nSelect options[1 - 3]:");
+    return 0;
+}
