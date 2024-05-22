bf2fcdd0d664282108ea27b1c8b3af288f199280
schizoneko <viettung11122004@gmail.com>
2024-05-22 19:39:19 +0700

    [ADD] Add input receiver

--- s1mpleCal/main.c
+++ s1mpleCal/main.c
@@ -1,4 +1,5 @@
 #include "stdio.h"
+#include "stdint-gcc.h"
 
 int main(){
     printf("\nSimple Calculator");
@@ -8,5 +9,10 @@ int main(){
     printf("\n3. Exit");
 
     printf("\nSelect options[1 - 3]:");
+
+
+    uint8_t option;
+    scanf("%hhd",&option);
+    printf("\nMode selected: %hhd",option);
     return 0;
 }
